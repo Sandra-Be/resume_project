6e10635c4fd567d687b609f824abe4515907448c
Sandra-Be <sandraberzk@gmail.com>
2021-06-11 12:32:23 +0000

    fixed error

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,6 +1,6 @@
 function fetchGitHubInformation(event) {
 
-    var username = $(#gh-username).val();
+    var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a Github username</h2>`);
         return;

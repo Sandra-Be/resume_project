dd4441aec25f5904d1dc644e433e0545bcd19290
Sandra-Be <sandraberzk@gmail.com>
2021-06-14 20:40:06 +0000

    code for email to be sent

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("service_d3u4bmq","template_2w9zdfq", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+}
